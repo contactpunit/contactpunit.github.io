5b394ce0aecd9edeee720080a10439d0bb836a7a
Punit Jain <contactpunitjain@gmail.com>
2020-05-14 23:06:19 +0530

    added flex

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,12 +5,51 @@ from __future__ import unicode_literals
 AUTHOR = 'Punit Jain'
 SITENAME = 'My Tech Blogs'
 SITEURL = ''
-
+SITETITLE = 'Punit Jain'
+SITESUBTITLE = 'Ideas and Thoughts'
 PATH = 'content'
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.6,
+        'indexes': 0.6,
+        'pages': 0.5,
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly',
+    }
+}
+SOCIAL = (
+    ('github', 'https://github.com/contactpunit'),
+    ('envelope', 'mailto:contactpunitjain@gmail.com'),
+    ('linkedin','https://np.linkedin.com/in/punit-jain-25302823'),
+)
+
+STATIC_PATHS = ['images', 'extra']
+MAIN_MENU = True
+MENUITEMS = (('Archives', '/archives'),('Categories', '/categories'),('Tags', '/tags'))
+PYGMENTS_STYLE = 'friendly'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = PAGE_URL + 'index.html'
+
+ARCHIVES_SAVE_AS = 'archives.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
 
 TIMEZONE = 'Asia/Kolkata'
 
 DEFAULT_LANG = 'en'
+
+THEME = 'themes/Flex'
+
+PLUGIN_PATHS = ['./pelican-plugins']
+
+PLUGINS = ['sitemap', 'post_stats', 'feed_summary']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
