a8e6266602ec506eada9b70a3e4be269d1e6a3df
Punit Jain <contactpunitjain@gmail.com>
2020-05-16 19:16:46 +0530

    Initial commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Punit Jain'
+#AUTHOR = 'Punit Jain'
 SITENAME = 'My Tech Blogs'
 SITEURL = ''
 SITETITLE = 'Punit Jain'
